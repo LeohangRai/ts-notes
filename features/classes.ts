2a0be25a752f056a8bbfb82cf51b9f01c74c0b18
Anup Rai <anup.rai@ekbana.info>
2023-03-16 08:14:42 +0545

    feat: constructors during inheritance

--- features/classes.ts
+++ features/classes.ts
@@ -45,3 +45,53 @@ class Car extends AutoMobile {
 const car = new Car();
 car.drive();
 car.honk();
+
+
+// Class constructors
+class Gadi {
+    color: string;
+
+    constructor(color: string) {
+        this.color = color;
+    }
+}
+
+const meroGadi = new Gadi('nilo');
+console.log(meroGadi.color);
+
+
+// Constructor parameter shortcut
+class Gadi2 {
+    constructor(public color: string) {}
+}
+
+const meroGadi2 = new Gadi2('rato');
+console.log(meroGadi2.color);
+
+
+
+// Constructors during Inheritance
+class PaymentService {
+    constructor(public url: string) {}
+
+    public getUrl(): string {
+        return this.url;
+    }
+}
+
+// Inheritance without overriding the parent class' constructor
+class EsewaService extends PaymentService {
+}
+
+const myEsewaService = new EsewaService('https://uat.esewa.com.np');
+console.log(myEsewaService.getUrl());
+
+
+// Inheritance and overriding the parent class' constructor
+class KhaltiService extends PaymentService {
+    constructor(private serviceCharnge: number, url: string) {
+        super(url);
+    }
+}
+
+const myKhaltiService = new KhaltiService(5, 'https://www.test.khalti-pay.com.np');
