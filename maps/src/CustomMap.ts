eb120fadace38f2dd7a231f08ef5ffc72d3024f6
Anup Rai <anup.rai@ekbana.info>
2023-03-18 19:00:39 +0545

    feat: invert dependency with interface

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,6 +1,5 @@
 import L from "leaflet";
-import { User } from "./User";
-import { Company } from "./Company";
+import { Mappable } from "./interfaces/Mappable";
 
 export class CustomMap {
     private map: L.Map;
@@ -8,17 +7,17 @@ export class CustomMap {
     constructor(divId: string) {
         this.map = new L.Map(divId, {
             center: {
-                lat: 27.7172,
-                lng: 85.3240
+                lat: 0,
+                lng: 0
             },
-            zoom: 3
+            zoom: 1
         });
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19
         }).addTo(this.map);
     }
 
-    addMarker(mappable: User | Company): void {
+    addMarker(mappable: Mappable): void {
         const marker = new L.Marker({
             lat: mappable.location.lat,
             lng: mappable.location.lng
