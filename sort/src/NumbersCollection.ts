3dff70ed7d108ba02f9c19bb3f095ae2555d891d
Anup Rai <anup.rai@ekbana.info>
2023-03-23 22:33:14 +0545

    refactor: refactor sorting logic into separate NumbersCollection class

---
+++ sort/src/NumbersCollection.ts
@@ -0,0 +1,21 @@
+export class NumbersCollection {
+
+    constructor(public data: number[]) {}
+
+    get length(): number {
+        return this.data.length;
+    }
+
+    compare(leftIndex: number, rightIndex: number): boolean {        
+        return this.data[leftIndex] > this.data[rightIndex];
+    }
+
+    swap(leftIndex: number, rightIndex: number): void {
+        const temp = this.data[leftIndex];
+        this.data[leftIndex] = this.data[rightIndex];
+        this.data[rightIndex] = temp;
+    }
+}
+
+const numCollection = new NumbersCollection([1, 0, -23, 8]);
+console.log(numCollection.length); // using the 'accessor' or getter method
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,28 +1,7 @@
-class Sorter {
-    constructor(public collection: number[] | string) {}
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
 
-    sort(): void {
-        const { length } = this.collection;
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-
-                if(this.collection instanceof Array){
-                    if(this.collection[j] > this.collection[j + 1]) {
-                        const temp = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = temp;
-                    }
-                }
-
-                if (typeof this.collection === 'string') {
-                    // perform string swapping somehow
-                }
-
-            }
-        } 
-    }
-}
-
-const sorter = new Sorter([10, 3, -5, 0]);
-sorter.sort();
-console.log(sorter.collection);
+const numbersCollection = new NumbersCollection([1, 0, -23, 8, -2]);
+const sorter = new Sorter(numbersCollection);
+sorter.sort()
+console.log(numbersCollection.data);
